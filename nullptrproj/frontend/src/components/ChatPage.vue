1f17ae87639cb76ec0c59c6cc437214bfc6c2c3b
Juan Francisco Sbruzzi <sbruzzi.juan@outlook.com>
2022-04-03 05:25:41 -0300

    Saco lo de jitsi del ''chat''

--- nullptrproj/frontend/src/components/ChatPage.vue
+++ nullptrproj/frontend/src/components/ChatPage.vue
@@ -7,34 +7,24 @@
 
 
 <script>
-import JitsiMeetJS from '@lyno/lib-jitsi-meet';
+
 import { defineComponent, ref } from 'vue'
 import options from '../options/config';
-
-JitsiMeetJS.init();
-JitsiMeetJS.setLogLevel(JitsiMeetJS.logLevels.INFO);
-const connection = new JitsiMeetJS.JitsiConnection(null, null, options);
-function onConnectionSuccess() {
-  console.log("onConnectionSuccess", arguments);
-}
-function onConnectionFailed() {
-  console.log("onConnectionFailed", arguments);
-}
-function disconnect() {
-  console.log("disconnect", arguments);
-}
-connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_ESTABLISHED, onConnectionSuccess);
-connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_FAILED, onConnectionFailed);
-connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_DISCONNECTED, disconnect);
-connection.connect();
+import Proxy from '../Proxy'
 
 
 export default defineComponent({
     name: 'ChatPage',
     setup() {
-        return {
-        }
+      const matches = ref([{}])
+      return {
+        matches
+      }
     },
+    async mounted() {
+      const res = Proxy.get('matches/')
+
+    }
     props: {
         
     }
